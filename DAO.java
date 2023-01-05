72448547019be6e577dc783172b0e9cef6799f64
nicolasdraperi <113093384+nicolasdraperi@users.noreply.github.com>
2023-01-05 08:43:46 +0100

    Update DAO.java

--- DAO.java
+++ DAO.java
@@ -37,20 +37,4 @@ public class DAO {
             }
         }
     }
-
-    public static List<Label> GetDevisByNum(String num, String id_client, String bien, String date, String objet) throws SQLException {
-        PreparedStatement ps = connect.prepareStatement("SELECT * FROM DEVIS WHERE DEV_NUMERO LIKE ? AND CLI_REF LIKE ? AND BIE_REF LIKE ? AND DEV_DATE LIKE ? AND DEV_OBJET LIKE ? ORDER BY DEV_NUMERO ASC");
-        ps.setString(1, "%"+num+"%");
-        System.out.println(ps);
-        ResultSet rs = ps.executeQuery();
-        List<Label> devis = new ArrayList<>();
-        while (rs.next()) {
-            Label devisLabel = new Label();
-            devisLabel.setText(rs.getString("DEV_NUMERO"));
-            devisLabel.setStyle("-fx-font-size: 15px;");
-            devisLabel.setPrefHeight(20);
-            devis.add(devisLabel);
-        }
-        return devis;
-    }
 }
