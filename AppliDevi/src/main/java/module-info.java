544e5186f087e1288ff00f665646e31940b505cc
SachaFougeras <113094009+SachaFougeras@users.noreply.github.com>
2023-01-06 16:54:33 +0100

    Delete AppliDevi directory

--- AppliDevi/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module sio.leo.applidevi {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-    requires java.sql;
-    opens sio.leo.applidevi to javafx.fxml;
-    exports sio.leo.applidevi;
-}
