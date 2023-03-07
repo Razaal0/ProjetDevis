2edbeb7037560615e1f3c0fe8df7116f00d19c80
Catia Paella <106531747+CatiaVarela@users.noreply.github.com>
2023-03-07 19:13:28 +0100

    Update App.java

--- ProjetDevis/Artizan/src/main/java/sio/leo/Artizan/App.java
+++ ProjetDevis/Artizan/src/main/java/sio/leo/Artizan/App.java
@@ -24,10 +24,14 @@ import sio.leo.Artizan.Modele.Poste;
 public class App extends Application {
 
     private Stage primaryStage;
-    private AnchorPane anchorpane;
     private ObservableList<Poste> tabledata = FXCollections.observableArrayList();
-
-    private static Scene scene;
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        this.primaryStage = stage;
+        this.primaryStage.setTitle("Article/Poste");
+        showPersonOverview();
+    }
     
     public App()throws SQLException{
         ArticlePosteDAO apdao = new ArticlePosteDAO();
@@ -39,35 +43,21 @@ public class App extends Application {
         public ObservableList<Poste> getTabledata(){
         return tabledata;
     }
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Application ");
-        Scene scene = new Scene(loadFXML("ArticlePost"), 640, 480);
-        primaryStage.setScene(scene);
-//        showPersonOverview();
-        primaryStage.show();
-    }
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    public void showPersonOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("ArticlePost.fxml"));
+            AnchorPane anchorpane = (AnchorPane) loader.load();
+            ArticleController controller = loader.getController();
+            controller.setApp(this);
+            Scene scene = new Scene(anchorpane);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-//    public void showPersonOverview() {
-//        try {
-//            FXMLLoader loader = new FXMLLoader();
-//            loader.setLocation(App.class.getResource("ArticlePost.fxml"));
-//            AnchorPane anchorpane = (AnchorPane) loader.load();
-//            ArticleController controller = loader.getController();
-//            controller.setApp(this);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     public static void main(String[] args) {
         launch();
