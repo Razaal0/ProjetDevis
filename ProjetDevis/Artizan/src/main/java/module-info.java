660c3c62726e0406ede5744f7dec42d3eaa54767
Catia Paella <106531747+CatiaVarela@users.noreply.github.com>
2023-04-05 17:04:58 +0200

    Delete ProjetDevis/Artizan directory

--- ProjetDevis/Artizan/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module sio.leo.Artizan {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-    requires java.sql;
-
-    opens sio.leo.Artizan to javafx.fxml;
-    exports sio.leo.Artizan;
-}
